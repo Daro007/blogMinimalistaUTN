e25b11b5c923ac65c9a21e6f215d5fc963a5e9f8
Dario Elias <darionicolaselias@gmail.com>
2020-04-28 12:22:17 +0200

    Guardando cambios

--- animacion.js
+++ animacion.js
@@ -2,24 +2,32 @@
 const youtube = document.getElementById("youtube");
 const twitter = document.getElementById("twitter");
 const insta = document.getElementById("instagram");
+const canvas = document.getElementById("canvas");
 
-youtube.addEventListener("mouseover", function() {
+youtube.addEventListener("mouseover", function () {
   youtube.classList.add("animacion");
 });
-youtube.addEventListener("mouseout", function() {
+youtube.addEventListener("mouseout", function () {
   youtube.classList.remove("animacion");
 });
 
-twitter.addEventListener("mouseover", function() {
+twitter.addEventListener("mouseover", function () {
   twitter.classList.add("animacion");
 });
-twitter.addEventListener("mouseout", function() {
+twitter.addEventListener("mouseout", function () {
   twitter.classList.remove("animacion");
 });
 
-insta.addEventListener("mouseover", function() {
+insta.addEventListener("mouseover", function () {
   insta.classList.add("animacion");
 });
-twitter.addEventListener("mouseout", function() {
+twitter.addEventListener("mouseout", function () {
   insta.classList.remove("animacion");
+});
+
+canvas.addEventListener("mouseover", function () {
+  canvas.classList.add("animacion");
+});
+canvas.addEventListener("mouseout", function () {
+  canvas.classList.remove("animacion");
 });
--- canvas.js
+++ canvas.js
@@ -1,8 +1,24 @@
-window.onload = function () {
-  let canvas = document.getElementById("canvas");
-  let context = canvas.getContext("2d");
+var exam = document.getElementById("canvas");
+var cxt = exam.getContext("2d");
 
-  let imagen = document.getElementById("imgCanvas");
+cxt.strokeStyle = "grey";
+cxt.lineWidth = 6; // grosor de la linea
+cxt.lineJoin = "round";
+cxt.moveTo(0, 0);
+cxt.lineTo(300, 120);
+cxt.lineTo(300, 0);
 
-  context.drawImage(imagen, 0, 0, canvas.width, canvas.height);
-};
+cxt.moveTo(0, 0);
+cxt.lineTo(20500, 300);
+
+cxt.moveTo(300, 0);
+cxt.lineTo(0, 120);
+// cxt.lineTo(0, 300);
+
+cxt.moveTo(0, 0);
+cxt.lineTo(0, 120);
+
+cxt.moveTo(600, 130);
+cxt.lineTo(-900, 100);
+
+cxt.stroke();
