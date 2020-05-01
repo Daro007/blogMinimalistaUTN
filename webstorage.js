46be5de03e31c7dc041066c46e199b23e920c5b7
Dario Elias <darionicolaselias@gmail.com>
2020-05-01 20:27:49 +0200

    Improving web storage

--- webstorage.js
+++ webstorage.js
@@ -1,17 +1,54 @@
-const item = document.getElementById("checklist");
-const alert = document.getElementById("alert");
+// const item = document.getElementById("checklist");
+// const alert = document.getElementById("alert");
 
-function guardar() {
-  sessionStorage.setItem("checklistUsuario", item.innerHTML);
-  alert.classList.remove("confirmacion");
-}
-function borrar() {
-  sessionStorage.clear();
-  location.reload();
+// function guardar() {
+//   localStorage.setItem("checklistUsuario", item.innerHTML);
+//   alert.classList.remove("confirmacion");
+// }
+// function borrar() {
+//   localStorage.clear();
+//   location.reload();
+// }
+
+// function mostrar() {
+//   if (localStorage.getItem("checklistUsuario")) {
+//     item.innerHTML = localStorage.getItem("checklistUsuario");
+//   }
+// }
+
+function base() {
+  const btnGuardar = document.getElementById("guardar");
+  btnGuardar.addEventListener("click", nuevoItem, false);
+  mostrarItem();
 }
 
-function mostrar() {
-  if (sessionStorage.getItem("checklistUsuario")) {
-    item.innerHTML = sessionStorage.getItem("checklistUsuario");
+function nuevoItem() {
+  const titulo = document.getElementById("tituloChecklist").value;
+  const checklist = document.getElementById("check").value;
+  const urgencia = document.getElementById("urgencia").value;
+  const fecha = document.getElementById("fecha").value;
+
+  const valores = [checklist, urgencia, fecha];
+  localStorage.setItem(titulo, valores);
+  console.log(checklist);
+  mostrarItem();
+}
+
+function mostrarItem() {
+  const caja = document.getElementById("caja");
+
+  for (let i = 0; i < localStorage.length; i++) {
+    var id = localStorage.key(i);
+    var valor = localStorage.getItem(id);
+
+    caja.innerHTML += "<ul><li>" + id + " - " + valor + "</li></ul>";
   }
 }
+
+function borrar() {
+  localStorage.clear();
+  location.reload();
+  mostrarItem();
+}
+
+window.addEventListener("load", base, false);
